a0625a6418e8d22bfa201a14f138898bc3332629
Mariam Ramy <mariamramy1074@gmail.com>
2026-06-28 03:04:38 +0300

    comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export type { Folder } from './Folder'
 export type { Sticker, StyleSettings, EnhancementSettings } from './Sticker.ts'
+
+//cleaner file for imports, so we can do import { Sticker } from './types' instead of import { Sticker } from './types/Sticker'
