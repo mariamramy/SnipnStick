cb93523bc63b0c61674989d0da537eb8a0f084fb
Mariam Ramy <mariamramy1074@gmail.com>
2026-07-04 23:25:05 +0300

    sticker service done and sticker types modified to allow nullification

--- src/types/Sticker.ts
+++ src/types/Sticker.ts
@@ -29,16 +29,16 @@ export interface Sticker {
 
     // image layers
     originalPng: Blob
-    bgRemovedRawPng: Blob    // straight out of the AI, before any brush
-    bgRemovedPng: Blob       // after user touch-up
+    bgRemovedRawPng: Blob | null   // straight out of the AI, before any brush
+    bgRemovedPng: Blob  | null     // after user touch-up
     enhancedPng: Blob | null
-    styledPng: Blob
+    styledPng: Blob | null
 
     // dimensions of the final styled image
     widthPx: number
     heightPx: number
 
     // Settings for each layer
-    styleSettings: StyleSettings;
+    styleSettings: StyleSettings | null;
     enhancementSettings: EnhancementSettings | null;
 }
